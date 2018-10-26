6920363e9da3706591b7f7d68ed045c9f2e6a062
Matthew <matthewmkco@gmail.com>
2018-10-26 15:33:39 +0100

    more execute functions

--- run.c
+++ run.c
@@ -29,8 +29,9 @@ int rdestination;
 uint32_t instruction;
 
 //Current Instruction Type
-//0 = N/A, 1 = Immediate, 2 = Unsigned, 3 = Register, 4 = Jump, 5 = Branch, 6 = Store
+//-1 = End, 1 = Immediate, 2 = Unsigned, 3 = Register, 4 = Jump, 5 = Branch, 6 = Store
 int instruction_type;
+char instruction_type_char;
 
 //Current opcode
 int opcode;
@@ -71,44 +72,37 @@ uint32_t addr;
 
 //add register and immediate
 int addi(int reg1){
-  printf("Instruction:Add Immediate\n");
   return reg1 + imm;
 }
 
 //subtract register and immediate
-int subi(uint32_t reg1, uint32_t immOperand){
-  printf("Instruction:Subtract Immediates\n");
-  return reg1 - immOperand;
-}
+// int subi(uint32_t reg1, uint32_t immOperand){
+//   return reg1 - immOperand;
+// }
 
 //bit wise and on register and immediate
 int andi(int reg1){
-  printf("Instruction:And Immediate\n");
   return (reg1 & imm);
 }
 
 //bit wise or on register and immediate
 int ori(int reg1){
-  printf("Instruction:Or Immediate\n");
   return (reg1 | imm);
 }
 
 //bit wise xor on register and immediate
 int xori(int reg1){
-  printf("Instruction:Xor Immediate\n");
   return (reg1 ^ imm);
 }
 
 //set less than immediate
 int slti(int reg1){
-  printf("Instruction:STLI\n");
   if(reg1 < imm)return 1;
   else return 0;
 }
 
 //set less than immediate unsigned
 int sltiu(int reg1){
-  printf("Instruction:STLIU\n");
   unsigned int reg1u = reg1;
   unsigned int immu = imm;
   if(reg1u < immu)return 1;
@@ -117,38 +111,32 @@ int sltiu(int reg1){
 
 //Load Upper Immediate
 int lui(){
-  printf("Instruction:Load Upper Immediate\n");
   return (imm & 0xFFFFF000);
 }
 
 //Add Upper Immediate to PC
 int auipc(){
-  printf("Instruction:Add Upper Immediate to PC\n");
   return((imm & 0xFFFFF000) + pc[0]);
 }
 
 //Add two registers
 int add(int reg1, int reg2){
-  printf("Instruction:Add\n");
   return (reg1 + reg2);
 }
 
 //Subtract two registers
 int sub(int reg1, int reg2){
-  printf("Instruction:Subtract\n");
   return (reg1 - reg2);
 }
 
 //Set if Less Than
 int slt(int reg1, int reg2){
-  printf("Instruction:SLT\n");
   if(reg1 < reg2)return 1;
   else return 0;
 }
 
 //Set if Less Than Unsigned
 int sltu(int reg1, int reg2){
-  printf("Instruction:SLTU\n");
   unsigned int reg2u = reg2;
   if(reg2u != 0)return 1;
   else return 0;
@@ -156,39 +144,33 @@ int sltu(int reg1, int reg2){
 
 //bit wise and on two registers
 int and(int reg1, int reg2){
-  printf("Instruction:And\n");
   return(reg1 & reg2);
 }
 
 //bit wise or on two registers
 int or(int reg1, int reg2){
-  printf("Instruction:Or\n");
   return(reg1 | reg2);
 }
 
 //bit wise xor on two registers
 int xor(int reg1, int reg2){
-  printf("Instruction:Xor\n");
   return(reg1 ^ reg2);
 }
 
 //logical left shift
 int sll(int reg1, int reg2){
-  printf("Instruction:Logical Left Shift\n");
   int shift = (reg2 & 0x1F);
   return(reg1 << shift);
 }
 
 //logical rigth shift
 int srl(int reg1, int reg2){
-  printf("Instruction:Logical Right Shift\n");
   int shift = (reg2 & 0x1F);
   return(reg1 >> shift);
 }
 
 //arithmetic roght shift
 int sra(int reg1, int reg2){
-  printf("Instruction:Logical Right Shift\n");
   int shift = (reg2 & 0x1F);
   return(reg1 >> shift);
 }
@@ -197,23 +179,170 @@ int sra(int reg1, int reg2){
 int jal(){
   printf("Instruction:Jump and Link\n");
   int originalPC = pc[0] + 4;
-  int jimm = (jimm1 + jimm2 + signedjimm1 + signedjimm2);
-  if((jimm & 0x00080000)){
-    jimm += 0xFFF00000;
-  }
-  pc[0] = pc[0] + (jimm);
+  pc[0] = pc[0] + (imm);
   return originalPC;
 }
 
 //jump and link register
-int jalr(){
-  
+int jalr(int reg1){
+  printf("Instruction:Jump and Link Register\n");
+  int originalPC = pc[0] + 4;
+  pc[0] = (imm + reg1) & 0b0;
+  return originalPC;
 }
 
 //branch if equal
-int beq(int reg1, int reg2){
+void beq(int reg1, int reg2){
   printf("Instruction:Branch if Equal\n");
+  if(reg1 == reg2){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
 
+//branch if not equal
+void bne(int reg1, int reg2){
+  printf("Instruction:Branch if Not Equal\n");
+  if(reg1 != reg2){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
+
+//branch if less than
+void blt(int reg1, int reg2){
+  printf("Instruction:Branch if Less Than\n");
+  if(reg1 < reg2){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
+
+//branch if less than
+void bltu(int reg1, int reg2){
+  printf("Instruction:Branch if Less Than Unsigned\n");
+  unsigned int reg1u = reg1, reg2u = reg2;
+  if(reg1u < reg2u){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
+
+//branch if greater than
+void bgt(int reg1, int reg2){
+  printf("Instruction:Branch if Greater Than\n");
+  if(reg1 >= reg2){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
+
+//branch if greater than
+void bgtu(int reg1, int reg2){
+  printf("Instruction:Branch if Greater Than Unsigned\n");
+  unsigned int reg1u = reg1, reg2u = reg2;
+  if(reg1u >= reg2u){
+    pc[0] = pc[0] + imm;
+  }
+  return;
+}
+
+//load memory into register
+int ld(char* cache, int reg1){
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  return *data;
+}
+
+//load 32-bit memory into register
+int lw(char* cache, int reg1){
+  printf("Instruction:Load\n");
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int data2 = ((*data) & 0xFFFFFFFF);
+  return data2;
+}
+
+//load 16-bit memory into register
+int lh(char* cache, int reg1){
+  printf("Instruction:Load\n");
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int data2 = ((*data) & 0x0000FFFF);
+  if(data2 & 0x00008000){
+    data2 = (data2 | 0xFFFF0000);
+  }
+  return data2;
+}
+
+//load unsigned 16-bit memory into register
+int lhu(char* cache, int reg1){
+  printf("Instruction:Load\n");
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int data2 = ((*data) & 0x0000FFFF);
+  return data2;
+}
+
+//load 8-bit memory into register
+int lb(char* cache, int reg1){
+  printf("Instruction:Load\n");
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int data2 = ((*data) & 0x000000FF);
+  if(data2 & 0x00000080){
+    data2 = (data2 | 0xFFFFFF00);
+  }
+  return data2;
+}
+
+//load unsigned 8-bit memory into register
+int lbu(char* cache, int reg1){
+  printf("Instruction:Load\n");
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int data2 = ((*data) & 0x000000FF);
+  return data2;
+}
+
+//store contents of register into memory
+void st(char* cache, int reg1, int reg2){
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  *data = reg2;
+}
+
+//store 32-bit value from low bits of register into memory
+void sw(char* cache, int reg1, int reg2){
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  *data = reg2;
+}
+
+//store 16-bit value from low bits of register into memory
+void sh(char* cache, int reg1, int reg2){
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int reg16bit = (reg2 & 0x0000FFFF);
+  *data = reg16bit;
+}
+
+//store 8-bit value from low bits of register into memory
+void sb(char* cache, int reg1, int reg2){
+  int offset = imm + reg1;
+  int* data;
+  data = (int*)(cache + offset);
+  int reg8bit = (reg2 & 0x000000FF);
+  *data = reg8bit;
 }
 
 // //multiply two registers
@@ -251,20 +380,6 @@ int nand(uint32_t reg1, uint32_t reg2){
   else return 0;
 }
 
-//load memory into register by returning
-uint32_t ld(char* cache, uint32_t offset){
-  uint32_t* data;
-  data = (uint32_t*)(cache + offset);
-  return *data;
-}
-
-//store contents of register into memory
-void st(uint32_t regVal, char* cache, uint32_t offset){
-  uint32_t* data;
-  data = (uint32_t*)(cache + offset);
-  *data = regVal;
-}
-
 //move value to register
 void mov(uint32_t* reg, uint32_t val){
   *reg = val;
@@ -281,27 +396,38 @@ void decode(){
   switch(opcode){
     case 0b0010011:
       instruction_type = 1;
+      instruction_type_char = 'I';
       break;
     case 0b0000011:
       instruction_type = 1;
+      instruction_type_char = 'I';
       break;
     case 0b0110111:
       instruction_type = 2;
+      instruction_type_char = 'U';
       break;
     case 0b0010111:
       instruction_type = 2;
+      instruction_type_char = 'U';
       break;
     case 0b0110011:
       instruction_type = 3;
+      instruction_type_char = 'R';
       break;
     case 0b1101111:
       instruction_type = 4;
+      instruction_type_char = 'J';
       break;
     case 0b1100011:
       instruction_type = 5;
+      instruction_type_char = 'B';
       break;
     case 0b0100011:
       instruction_type = 6;
+      instruction_type_char = 'S';
+      break;
+    default:
+      instruction_type = -1;
       break;
   }
 
@@ -321,8 +447,7 @@ void decode(){
     else{
       imm = (instruction & 0xFFF00000) >> 20;
       if((imm & 0x00000800)){
-        printf("Sign Extension\n");
-        imm += 0xFFFFF000;
+        imm = (imm | 0xFFFFF000);
       }
     }
   }
@@ -342,172 +467,211 @@ void decode(){
   //J type instructions
   else if(instruction_type == 4){
     rdestination = (instruction & 0x00000F80) >> 7;
-    jimm1 = (instruction & 0x000FF000) >> 12 << 12;
-    signedjimm1 = (instruction & 0x001FF000) >> 20 << 11;
-    jimm2 = (instruction & 0x7FE00000) >> 21 << 1;
-    signedjimm2 = (instruction & 0x80000000) >> 31 << 20;
+    imm = (((instruction & 0x000FF000) >> 12 << 12) | ((instruction & 0x001FF000) >> 20 << 11) | ((instruction & 0x7FE00000) >> 21 << 1) | ((instruction & 0x80000000) >> 31 << 20));
+    if((imm & 0x00080000)){
+      imm = (imm | 0xFFF00000);
+    }
   }
   //B type instructions
   else if(instruction_type == 5){
-    signedbimm1 = (instruction & 0x00000080) >> 7;
-    bimm1 = (instruction & 0x00000F00) >> 8;
     funct3 = (instruction & 0x00007000) >> 12;
     rsource1 = (instruction & 0x000F8000) >> 15;
     rsource2 = (instruction & 0x01F00000) >> 20;
-    bimm2 = (instruction & 0x7E000000) >> 25;
-    signedbimm2 = (instruction & 0x10000000) >> 31;
+    imm = (((instruction & 0x00000080) >> 7 << 11) | ((instruction & 0x00000F00) >> 8 << 1) | ((instruction & 0x7E000000) >> 25 << 5) | ((instruction & 0x10000000) >> 31 << 5));
   }
   //S type instructions
   else if(instruction_type == 6){
-    simm1 = (instruction & 0x00000F80) >> 7;
     funct3 = (instruction & 0x00007000) >> 12;
     rsource1 = (instruction & 0x000F8000) >> 15;
     rsource2 = (instruction & 0x01F00000) >> 20;
-    simm2 = (instruction & 0xFE000000) >> 25;
+    imm = (((instruction & 0x00000F80) >> 7) | ((instruction & 0xFE000000) >> 25 << 5));
   }
 }
-
-// void decode(){
-//   opcode = (instruction & 0xFC000000) >> 26;
-//   switch(opcode){
-//     case 0x0:
-//       instruction_type = 1;
-//       break;
-//     case 0x02:
-//       instruction_type = 3;
-//       break;
-//     case 0x03:
-//       instruction_type = 3;
-//       break;
-//     case 0x04:
-//       instruction_type = 2;
-//       break;
-//     case 0x05:
-//       instruction_type = 2;
-//       break;
-//     case 0x06:
-//       instruction_type = 2;
-//       break;
-//     case 0x08:
-//       instruction_type = 2;
-//       break;
-//     case 0x09:
-//       instruction_type = 2;
-//       break;
-//     case 0x0A:
-//       instruction_type = 2;
-//       break;
-//     case 0x0B:
-//       instruction_type = 2;
-//       break;
-//     case 0x0C:
-//       instruction_type = 2;
-//       break;
-//     case 0x0D:
-//       instruction_type = 2;
-//       break;
-//     case 0x0F:
-//       instruction_type = 2;
-//       break;
-//     // case 0x10:
-//     //   instruction_type = 1;
-//     //   break;
-//     case 0x23:
-//       instruction_type = 2;
-//       break;
-//     case 0x24:
-//       instruction_type = 2;
-//       break;
-//     case 0x25:
-//       instruction_type = 2;
-//       break;
-//     case 0x28:
-//       instruction_type = 2;
-//       break;
-//     case 0x29:
-//       instruction_type = 2;
-//       break;
-//     case 0x2B:
-//       instruction_type = 2;
-//       break;
-//     default:
-//       printf("Error in decode stage\n");
-//       printf("Opcode was:%d\n", opcode);
-//       separator;
-//       exit(1);
-//   }
-//
-//   if(instruction_type == 1){
-//     rsource = (instruction & 0x03E00000) >> 21;
-//     rtarget = (instruction & 0x001F0000) >> 16;
-//     rdestination = (instruction & 0x0000F800) >> 11;
-//     shamt = (instruction & 0x000007C0) >> 6;
-//     funct = (instruction & 0x0000003F);
-//   }
-//   else if(instruction_type == 2){
-//     rsource = (instruction & 0x03E00000) >> 21;
-//     rtarget = (instruction & 0x001F0000) >> 16;
-//     imm = (instruction & 0x0000FFFF);
-//   }
-//   else if(instruction_type == 3){
-//     printf("Not done yet\n");
-//     exit(1);
-//   }
-//
-//   switch(instruction_type){
-//     case 0:
-//       break;
-//     case 1:
-//       printf("Instruction format:R\n");
-//       break;
-//     case 2:
-//       printf("Instruction format:I\n");
-//       break;
-//     case 3:
-//       printf("Instruction format:J\n");
-//       break;
-//   }
-//   printf("Opcode:%d\n", opcode);
-// }
-
-// execute_rformat(){
-//   switch(funct){
-//     case 0x18:
-//       registers[rdestination] = mul(registers[rsource], registers[rtarget]);
-//     case 0x1A:
-//       registers[rdestination] = div(registers[rsource], registers[rtarget]);
-//     case 0x20:
-//       registers[rdestination] = add(registers[rsource], registers[rtarget]);
-//     case 0x22:
-//       registers[rdestination] = sub(registers[rsource], registers[rtarget]);
-//   }
-// }
 
 //execute I format instructions
 void execute_iformat(){
+  if(opcode == 0b0010011){
+    switch(funct3){
+      case(0b000):
+      printf("Instruction:Add Immediate\n");
+      registers[rdestination] = addi(registers[rsource1]);
+      break;
+      case(0b111):
+      printf("Instruction:And Immediate\n");
+      registers[rdestination] = andi(registers[rsource1]);
+      break;
+      case(0b110):
+      printf("Instruction:Or Immediate\n");
+      registers[rdestination] = ori(registers[rsource1]);
+      break;
+      case(0b100):
+      printf("Instruction:Xor Immediate\n");
+      registers[rdestination] = xori(registers[rsource1]);
+      break;
+      case(0b010):
+      printf("Instruction:STLI\n");
+      registers[rdestination] = slti(registers[rsource1]);
+      break;
+      case(0b011):
+      printf("Instruction:STLIU\n");
+      registers[rdestination] = sltiu(registers[rsource1]);
+      break;
+      //TODO:still have shift functions to create
+    }
+  }
+  else if(opcode == 0b0000011){
+    switch(funct3){
+      case(0b000):
+        printf("Instruction:Load 8-bit\n");
+        registers[rdestination] = lb(Dcache, registers[rsource1]);
+        break;
+      case(0b001):
+        printf("Instruction:Load 16-bit\n");
+        registers[rdestination] = lh(Dcache, registers[rsource1]);
+        break;
+      case(0b010):
+        printf("Instruction:Load 32-bit\n");
+        registers[rdestination] = lw(Dcache, registers[rsource1]);
+        break;
+      case(0b100):
+        printf("Instruction:Load 8-bit Unsigned\n");
+        registers[rdestination] = lbu(Dcache, registers[rsource1]);
+        break;
+      case(0b101):
+        printf("Instruction:Load 16-bit Unsigned\n");
+        registers[rdestination] = lhu(Dcache, registers[rsource1]);
+        break;
+    }
+  }
+  return;
+}
+
+void execute_uformat(){
+  switch(opcode){
+    case(0b0110111):
+      printf("Instruction:Load Upper Immediate\n");
+      lui();
+      break;
+    case(0b0010111):
+      printf("Instruction:Add Upper Immediate to PC\n");
+      auipc();
+      break;
+  }
+  return;
+}
+
+void execute_rformat(){
   switch(funct3){
     case(0b000):
-      registers[rdestination] = addi(registers[rsource1]);
+      if(funct7 == 0b0000000){
+        printf("Instruction:Add\n");
+        add(registers[rsource1], registers[rsource2]);
+      }
+      else if(funct7 == 0b0100000){
+        printf("Instruction:Subtract\n");
+        sub(registers[rsource1], registers[rsource2]);
+      }
+      break;
+    case(0b001):
+      printf("Instruction:Logical Left Shift\n");
+      sll(registers[rsource1], registers[rsource2]);
+      break;
+    case(0b010):
+      printf("Instruction:SLT\n");
+      slt(registers[rsource1], registers[rsource2]);
+      break;
+    case(0b011):
+      printf("Instruction:SLTU\n");
+      sltu(registers[rsource1], registers[rsource2]);
+      break;
+    case(0b100):
+      printf("Instruction:Xor\n");
+      xor(registers[rsource1], registers[rsource2]);
+      break;
+    case(0b101):
+      if(funct7 == 0b0000000){
+        printf("Instruction:Logical Right Shift\n");
+        srl(registers[rsource1], registers[rsource2]);
+      }
+      else if(funct7 == 0b0100000){
+        printf("Instruction:Logical Right Shift\n");
+        sra(registers[rsource1], registers[rsource2]);
+      }
+      break;
+    case(0b110):
+      printf("Instruction:Or\n");
+      or(registers[rsource1], registers[rsource2]);
+      break;
+    case(0b111):
+      printf("Instruction:And\n");
+      and(registers[rsource1], registers[rsource2]);
+      break;
   }
+  return;
+}
+
+void execute_jformat(){
+  return;
+}
+
+void execute_bformat(){
+  return;
+}
+
+void execute_sformat(){
+  switch(funct3){
+    case(0b000):
+      printf("Instruction:Store 8-bit\n");
+      sb(Dcache, registers[rsource1], registers[rsource2]);
+      break;
+    case(0b001):
+      printf("Instruction:Store 16-bit\n");
+      sh(Dcache, registers[rsource1], registers[rsource2]);
+      break;
+    case(0b010):
+      printf("Instruction:Store 32-bit\n");
+      sw(Dcache, registers[rsource1], registers[rsource2]);
+      break;
+  }
+  return;
 }
 
 void execute_sj(){
+  if(instruction_type == 4){
+    execute_jformat();
+  }
+  else if(instruction_type == 7){
+    execute_sformat();
+  }
   return;
 }
 
 void execute(){
-  printf("Instruction Type:%d\n", instruction_type);
-  if(instruction_type == 5 || instruction_type == 6){
+  if(instruction_type == -1){
+    printf("End of Program\n");
+    exit(1);
+  }
+  printf("Instruction Type:%c\n", instruction_type_char);
+  if(instruction_type == 4 || instruction_type == 6){
     execute_sj();
     return;
   }
 
   if(rdestination == 0){
-    printf("Error, register destination cannot be written into");
+    printf("Error, register destination cannot be written into\n");
     exit(1);
   }
   if(instruction_type == 1){
     execute_iformat();
+  }
+  else if(instruction_type == 2){
+    execute_uformat();
+  }
+  else if(instruction_type == 3){
+    execute_rformat();
+  }
+  else if(instruction_type == 5){
+    execute_bformat();
   }
   return;
 }
@@ -553,7 +717,7 @@ void set_register(){
     registers[i] = 0;
   }
   for(int i = 0; i < SIZE; i++){
-    st(-1, Icache, i);
+    st(Icache, i, -1);
   }
 }
 
@@ -561,12 +725,13 @@ void load_program(char* file){
   FILE* fp;
   fp = fopen(file, "r");
   unsigned int tinstruction, loadpc = 0;
+  imm = 0;
   if(fp == NULL){
     printf("Error in loading program, file is null");
     exit(1);
   }
   while(fscanf(fp, "%x", &tinstruction) != EOF){
-    st(tinstruction, Icache, loadpc);
+    st(Icache, loadpc, tinstruction);
     loadpc = loadpc + 4;
   }
 
@@ -582,7 +747,14 @@ int main(int argc, char** argv){
 
 
   set_register();
+  separator;
+  printf("Loading Program\n");
   load_program(argv[1]);
+  // for(int i = 0; i < 20; i++){
+  //   printf("%x\n", Icache[i]);
+  // }
+  printf("%x\n",Icache[0]);
+  separator;
 
   run();
 
